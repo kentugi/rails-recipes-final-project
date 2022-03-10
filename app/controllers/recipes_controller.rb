22677f3519a4501385fb420c42a79e16869f8b57
Andrey Kryuchkov <askryuchkov@ya.ru>
2022-03-10 12:42:22 +0800

    search bar with one keyword done

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,10 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = Recipe.all
+    if params[:recipe]
+      @recipes = Recipe.search_by_label(params[:recipe][:label])
+    else
+      @recipes = Recipe.all
+    end
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-This file should contain all the record creation needed to seed the database with its default values.
-The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-
-Examples:
-
-  movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-  Character.create(name: 'Luke', movie: movies.first)
-
 
 RecipeIngredient.destroy_all
 Bookmark.destroy_all
@@ -78,10 +70,13 @@ end
   review.save!
 end
 
-ri = RecipeIngredient.new(
-  recipe: Recipe.first,
-  ingredient: Ingredient.first
-)
-ri.save
+
+Ingredient.all.each do |i|
+  ri = RecipeIngredient.new(
+    recipe: Recipe.all.sample,
+    ingredient: i
+  )
+  ri.save
+end
 
 puts 'Finished!'
